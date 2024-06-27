258a01cede2419112cbd6edd954677adc521d8cb
RyanTheTurian <ryan.soso@studenti.unipd.it>
2024-06-27 18:00:18 +0200

    story

--- NPC.java
+++ NPC.java
@@ -1,6 +1,7 @@
 public class NPC extends Character {
     private String mapPiece;
-
+    private boolean hide=true;
+    private boolean interact=false;
     NPC(String id, int mH, int d, String mapPiece) {
         super(id, mH, d);
         this.mapPiece = mapPiece;
@@ -8,5 +9,17 @@ public class NPC extends Character {
 
     public String interact() {
         return mapPiece;
+    }
+    public boolean getHide() {
+    	return hide;
+    }
+    public boolean getInteract() {
+    	return interact;
+    }
+    public void setHide(){
+    	hide=!hide;
+    }
+    public void setInteract() {
+    	interact=!interact;
     }
 }
--- Story.java
+++ Story.java
@@ -7,4 +7,7 @@ public class Story {
 		System.out.println("Hello "+x+"! \nYou are an adventurer who has woken up on the beach of an island that seems abandoned...\n"
 				+ "You don't know how you ended up here, but you feel that you have to leave as soon as possible.\nExplore and interact to try to figure out how to escape\n");
 	}
+	public void seaCreatureDialogue() {
+		System.out.println("Be careful, be careful...this beach is not safe. A sea creature just eat my boat and i think it killed the fisherman\nIt hides in the deep sea, if you help me killing him I'll give you a reward");
+	}
 }
