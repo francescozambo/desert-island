b958cc20bfd0726ed5178a42cc8f30e1a9521a02
RyanTheTurian <ryan.soso@studenti.unipd.it>
2024-06-28 11:27:20 +0200

    Update Story.java

--- Story.java
+++ Story.java
@@ -10,9 +10,6 @@ public class Story {
 		System.out.println("Hello "+x+"! \n\nYou are an adventurer who has woken up on the beach of an island that seems abandoned...\n"
 				+ "You don't know how you ended up here, but you feel that you have to leave as soon as possible.\nExplore and interact to try to figure out how to escape\n");
 	}
-	public void seaCreatureDialogue() {
-		System.out.println("Be careful, be careful...this beach is not safe. A sea creature just eat my boat and i think it killed the fisherman\nIt hides in the deep sea, if you help me killing him I'll give you a reward");
-	}
 	
 	public void firtsInteractionBeach(DesertIsland ds,Player player, NPC npc) {
 		if(player.getInventory().findItem(player.getInventory().getItemById("Showel"))==true) {
