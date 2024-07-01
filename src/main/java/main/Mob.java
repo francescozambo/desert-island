6d81fa0512ebd186507de0d6c637044d26c5f161
josh3lChivo <joshua.sepulveda@studenti.unipd.it>
2024-07-01 11:41:19 +0000

    a

--- src/main/java/main/Mob.java
+++ src/main/java/main/Mob.java
@@ -1,10 +1,23 @@
 package main;
 
 public class Mob extends Character{
-	int action=0;
+	int actioncure=5;
+	final int fleeMalus =3;
+	final int smallAttack =6;
 	Mob(String id, int mH, int d) {
 		super(id, mH, d);
 	}
-	
+	public void flee(Player player) {
+		player.giveHealth(fleeMalus);
+	}
+	public void attack(Player player) {
+		player.removeHealth(damage);
+	}
+	public void smallAttack(Player player) {
+		player.removeHealth(smallAttack);
+	}
+	public void cure() {
+		this.giveHealth(actioncure);
+	}
 
 }
