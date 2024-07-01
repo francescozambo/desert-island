515e2d71d7046292cc93cc1659f2a3593a1519ac
Joshua Sepulveda <joshua.sepulveda@studenti.unipd.it>
2024-07-01 14:50:09 +0000

    change

--- src/main/java/main/Story.java
+++ src/main/java/main/Story.java
@@ -13,7 +13,7 @@ public class Story {
 	public void beginnin(String x) {
 		System.out.println("Hello "+x+"! \n\nYou are an adventurer who has woken up on the beach of an island that seems abandoned...\n"
 				+ "You don't know how you ended up here, but you feel that you have to leave as soon as possible.\nExplore and interact to try to figure out how to escape\n"
-				+ "		   |\n" + //
+				+ "|\n" + //
 										"         \\ _ /\r\n" + //
 										"       -= (_) =-\r\n" + //
 										"         /   \\         _\\/_\r\n" + //
